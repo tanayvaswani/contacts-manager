3c0e350115d8911047a4f5603288ec2513ba2015
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-16 04:37:52 +0530

    CRUD - updateContact

--- api/controllers/contactController.js
+++ api/controllers/contactController.js
@@ -46,7 +46,19 @@ const getContact = asyncHandler(async (req, res) => {
 // @route PUT/api/contacts/id
 // @access public
 const updateContact = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update contact with id: ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found!");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
 });
 
 // @desc Delete contact with ID

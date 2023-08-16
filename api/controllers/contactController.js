9055442e99173f009a65cc3a1fc69a434212191d
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-16 17:53:14 +0530

    PUT & DELETE logic done for logged in users

--- api/controllers/contactController.js
+++ api/controllers/contactController.js
@@ -54,6 +54,13 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found!");
   }
 
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error(
+      "User don't have permission to update other user's contacts."
+    );
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -73,8 +80,15 @@ const deleteContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found!");
   }
 
-  await Contact.deleteOne();
-  res.status(200).json({ message: `Deleted contact!` }, contact);
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error(
+      "User don't have permission to delete other user's contacts."
+    );
+  }
+
+  await Contact.deleteOne({ _id: req.params.id });
+  res.status(200).json(contact);
 });
 
 module.exports = {
